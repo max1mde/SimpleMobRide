ef4e924eec62b81e4bbed0adcc8250dd0558f213
Maxim <114857048+JavaDevMC@users.noreply.github.com>
2022-10-14 21:11:53 +0200

    Update SimpleMobRide.java

--- src/dev/maximde/simplemobride/main/SimpleMobRide.java
+++ src/dev/maximde/simplemobride/main/SimpleMobRide.java
@@ -8,17 +8,17 @@ import dev.maximde.simplemobride.events.EntityClick;
 public class SimpleMobRide extends JavaPlugin {
 	
 	/**
-	 * Method is called on plugin enable
+	 * Method is called on plugin start
 	 */
 	@Override
 	public void onEnable() {
 		registerEvents();
-    	getServer().getConsoleSender().sendMessage("§5==>> Simple Mob Ride <<==");
-    	getServer().getConsoleSender().sendMessage("§5=========================");
-    	getServer().getConsoleSender().sendMessage("§5==Plugin by MaximDe======");
-    	getServer().getConsoleSender().sendMessage("§bhttps://discord.gg/gbqF32Qsv2");
-    	getServer().getConsoleSender().sendMessage("§bhttps://www.spigotmc.org/members/maximde.1620695/");
-    	getServer().getConsoleSender().sendMessage("§5=========================");
+    	getServer().getConsoleSender().sendMessage("Â§5==>> Simple Mob Ride <<==");
+    	getServer().getConsoleSender().sendMessage("Â§5=========================");
+    	getServer().getConsoleSender().sendMessage("Â§5==Plugin by MaximDe======");
+    	getServer().getConsoleSender().sendMessage("Â§bhttps://discord.gg/gbqF32Qsv2");
+    	getServer().getConsoleSender().sendMessage("Â§bhttps://www.spigotmc.org/members/maximde.1620695/");
+    	getServer().getConsoleSender().sendMessage("Â§5=========================");
 	}
 	
 	/**
